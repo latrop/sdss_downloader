521fd7ebeaabc7d6be375130e06ac822526afb4b
latrop <savchenko.s.s@gmail.com>
2013-04-01 19:52:11 +0400

    Downloading of all fields in the given radius is added; Readme is added

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-#####
+This program downloads fits files with fields from SDSS DR9 for specified coordinates.
+It needs only Python >= 2.6 to run, nothing else.
+
+
+INSTALLATION
+
+The program does not require any installation process. Just download and unpack 
+it anywhere you want.
+
+
+USAGE
+
+    The program requires a table with coordinates of desired objects. The table
+must contain three columns: the first one -- names of objects (they need not to belong to
+any catalogue, just string for filenames, it has to be unique),
+the second one -- RA in degrees, the third one -- DEC in degrees. By default the name of this
+file is 'coordinates.dat', but you can specify an arbitrary name (see KEYS section).
+    
+    The only one comand line argument that is needed is a set of filters. The set of 
+filters must by specified as a solid string of lowercase letters, for example urz or ugriz.
+
+
+KEYS
+
+    -i, --input FILENAME       specified a file with table of coordinates (in case of its name is not 'coordinates.dat')
+
+    -r, --radius R             If object is located close to a edge of a field it may
+                               be splitted onto two or more fields. Setting the --r
+			       parameter force the program to search and download
+			       all fields witin radius R arcminutes around the object.
+			       Note that about one-third of SDSS is overlapped by
+			       two or more fields and with -r parameter the program will
+			       download them all. The maximum value of R is 60 arcminutes.
+			       Be carefull with big values of R: it may take a large
+			       ammount of disk space.
+
+    -s, --script	       If you turn the -r parameter on, you will recieve several
+    	 		       fields for some objects (they will be stored in different
+			       files with _0, _1, _2... endings). You may want to concatenate
+			       all this pieces into one image by using Emmanuel Bertins SWarp 
+			       program (http://www.astromatic.net/software/swarp). With -s
+			       parameter turned on, the program generates a simpe Bash script
+			       which runs the SWarp program for all this fields. Just run it
+			       when downloading is completed (the script takes the downloaded
+			       files as it is, you must not rename or unpack them). SWarp package
+			       have to be installed on your system.
+
+
+OUTPUT
+
+The fields will be downloaded in the 'downloads' directory, each filter in separate subdirectory
+(downloads/g, downloads/r, etc.). SWarp-scripts (if -s option is activated) will appear
+in the same directory with downloaded files.
+
+   The program generates also two ASCII files: 'fiedls.dat' with lists of the downloaded fields
+for each object and 'errors_404.dat' with a list of objects which have not been downloaded
+due to "not foun" or "connection" errors.
+
+
+EXAMPLES
+
+    python ./sdss_downloader.py gri -i mygalaxies.dat -r 0.5 -s
+
+downloades all fields within 0.5 arcminutes for objects from file 'mygalaxies.dat' 
+in filters g, r and i.
--- sdss_downloader.py
+++ sdss_downloader.py
@@ -1,15 +1,10 @@
 #! /usr/bin/env python
 
-################################################################################################
+
+#                                
 #  Author: Sergey Savchenko (savchenko.s.s@gmail.com)
-#                                                                                              #
-#  Input: file "coordinates.dat" with two columns RA and DEC in degrees. One row per galaxy.   #
-#                                                                                              #
-#  Using: first command line argument is a string with list of filters.                        #
-#                                                                                              #
-#  Example: $> downloader_dr9_th.py gri                                                        #
-#                                                                                              #
-################################################################################################
+#
+
 
 from threading import Thread
 
@@ -19,22 +14,47 @@ import urllib2
 import sys
 import os
 
-def findField2(objRA, objDEC):
-    radius = 30
-    request = "http://skyserver.sdss3.org/dr9/en/tools/search/x_radial.asp?ra=%1.5f&dec=%1.5f&radius=10&min_u=0&max_u=20&min_g=0&max_g=20&min_r=0&max_r=20&min_i=0&max_i=20&min_z=0&max_z=20&entries=top&topnum=10&format=csv" % (objRA, objDEC)
+import argparse
+
+def findField2(objRA, objDEC, radius):
+    if radius < 0.001:
+        request = "http://skyserver.sdss3.org/dr9/en/tools/search/x_radial.asp?ra=%1.5f&dec=%1.5f&radius=2&min_u=0&max_u=20&min_g=0&max_g=20&min_r=0&max_r=20&min_i=0&max_i=20&min_z=0&max_z=20&entries=top&topnum=10&format=csv" % (objRA, objDEC)
+    else:
+        request = "http://skyserver.sdss3.org/dr9/en/tools/search/x_radial.asp?ra=%1.5f&dec=%1.5f&radius=%1.2f&min_u=0&max_u=20&min_g=0&max_g=20&min_r=0&max_r=20&min_i=0&max_i=20&min_z=0&max_z=20&entries=top&topnum=500&format=csv" % (objRA, objDEC, radius)
     u = urllib2.urlopen(request)
     table = u.read().split("\n")
+    # Find the nearest object and the corresponding field
     minDist = 1e10
     for line in table[1:-1]:
-        objid, run, rerun, camcol, field, obj, t, ra, dec, u, g, r, i, z, E_u, E_g, E_r, E_i, Err_z = [float(i) for i in line.split(',')]
-        dist = hypot(objRA -ra, objDEC - dec)
+        objParams = line.split(',')
+        objID  = int(objParams[0])
+        run    = int(objParams[1])
+        rerun  = int(objParams[2])
+        camcol = int(objParams[3])
+        field  = int(objParams[4])
+        ra     = float(objParams[7])
+        dec    = float(objParams[8])
+        dist   = hypot(objRA -ra, objDEC - dec)
         if dist < minDist:
-            minDist = int(dist)
-            optRun = int(run)
-            optRerun = int(rerun)
-            optCamcol = int(camcol)
-            optField = int(field)
-    return optRun, optRerun, optCamcol, optField
+            minDist = dist
+            optObjID = objID
+            optRun = run
+            optRerun = rerun
+            optCamcol = camcol
+            optField = field
+    fList = [(optRun, optRerun, optCamcol, optField)]
+    if radius < 0.001:
+        return fList, optObjID
+    else:
+        for line in table[1:-1]:
+            objParams = line.split(',')
+            run    = int(objParams[1])
+            rerun  = int(objParams[2])
+            camcol = int(objParams[3])
+            field  = int(objParams[4])
+            if (run, rerun, camcol, field) not in fList:
+                fList.append((run, rerun, camcol, field))
+    return fList, optObjID
 
 def getUrl(run, rerun, camcol, field, band):
     return "http://data.sdss3.org/sas/dr9/boss/photoObj/frames/%s/%i/%i/frame-%s-%.6i-%i-%.4i.fits.bz2" % (
@@ -55,8 +75,6 @@ def testUrlExists(url):
         
 
 def download(url, passband, file_name):
-    if file_name is None:
-        file_name = url.split('/')[-1].split('.')[0]
     try:
         u = urllib2.urlopen(url)
     except urllib2.HTTPError:
@@ -75,73 +93,112 @@ def threadsAlive(listOfThreads):
             return True
     return False
 
+parser = argparse.ArgumentParser(description="Download fits-files of fields for specified coordinates")
+parser.add_argument("filters", help="List of filters (for example gri or uiz)")
+parser.add_argument("-i", "--input", default="coordinates.dat", help="File with coordinates to download")
+parser.add_argument("-r", "--radius", default=0.0, help="Download ajacent fields if any exist in r arcmin (max 60 arcmin)", type=float)
+parser.add_argument("-s", "--script", action="store_true", default=False, help="Create a script for concatenation by SWarp.")
+args = parser.parse_args()
 
-bandlist = sys.argv[1]
+if args.radius > 60:
+    print "Radius must by less than 60 arcmin"
+    sys.exit(1)
 
+bandlist = args.filters
 for band in bandlist:
     if not os.path.exists("./downloads/%s" % (band)):
         os.makedirs("./downloads/%s" % (band))
 
-listOfCoords = open("coordinates.dat").readlines()
+if args.script:
+    for band in bandlist:
+        f = open("./downloads/%s/concatenate.sh" % (band), "w")
+        f.truncate(0)
+        f.close()
+
+
+listOfCoords = open(args.input).readlines()
 counter = 0
 errFile = open("errors_404.dat", "w", buffering=0)
 errFile.truncate(0)
-with open("coords2fields.dat", "w", buffering=0) as outFile:
+with open("fields.dat", "w", buffering=0) as outFile:
     outFile.truncate(0)
     for line in listOfCoords:
         startTime = time()
         counter += 1
         params = line.split()
+
         if len(params) == 3:
             galName = params[0]
             ra = float(params[1])
             dec = float(params[2])
             print "\033[33m Downloading field for %1.5f %1.5f: '%s'  (%i/%i) \033[0m" % (ra, dec, galName, counter, len(listOfCoords))
         else:
-            ra = float(params[0])
-            dec = float(params[1])
-            galName = None
-            print "\033[33m Downloading field for %1.5f %1.5f  (%i/%i) \033[0m" % (ra, dec, counter, len(listOfCoords))
-        run, rerun, camcol, field = findField2(ra, dec)
-        # Check if fits files exist for all filters:
-        print " Checking file existense:"
-        allExist = True
-        urls = {}
-        for band in bandlist:
-            print "                    %s" % (band),
-            url = getUrl(run, rerun, camcol, field, band)
-            answer = testUrlExists(url)
-            if answer == -1:
-                allExist = False
-                print "\033[31m [Fail!] \033[0m\n"
-                break
-            print "\033[32m [OK] \033[0m   (%i bytes)" % (answer)
-            urls[band] = url
-        if not allExist:
-            errFile.write("%1.6f  %1.6f   %s \n" % (ra, dec, url.split("/")[-1][8:-9]))
-            continue
-        downloadThreads = []
-        for band in bandlist:
-            dth = Thread(target=download, args=(urls[band], band, galName))
-            dth.daemon = True
-            dth.start()
-            downloadThreads.append(dth)
-        print " Downloading",
-        while threadsAlive(downloadThreads):
-            sys.stdout.write(".")
-            sys.stdout.flush()
-            sleep(0.333)
-            sys.stdout.write(".")
-            sys.stdout.flush()
-            sleep(0.333)
-            sys.stdout.write(".")
-            sys.stdout.flush()
-            sleep(0.333)
-            sys.stdout.write("\b\b\b   \b\b\b")
-            sys.stdout.flush()
-            sleep(0.333)
-        if galName is None:
-            galName = url.split("/")[-1][8:-9]
-        print "         \033[34m [DONE] \033[0m    (%1.2f sec)\n\n" % (time()-startTime)
-        outFile.write("%1.6f  %1.6f   %s \n" % (ra, dec, galName))
+            print "%s must contain three columns: name, RA and DEC" % (args.input)
+            sys.exit(1)
+        objFieldList, objID = findField2(ra, dec, args.radius)
 
+        if len(objFieldList) > 1:
+            print "There are %i files for this object" % (len(objFieldList))
+        outFile.write("%s  %1.6f  %1.6f  " % (galName, ra, dec))
+        for ifield in xrange(len(objFieldList)):
+            if len(objFieldList) > 1:
+                print "Downloading (%i/%i)" % (ifield + 1, len(objFieldList))
+                curGalName = galName + "_" + str(ifield)
+            else:
+                curGalName = galName
+
+            run, rerun, camcol, field = objFieldList[ifield]
+            # Check if fits files exist for all filters:
+            print " Checking file existense:"
+            allExist = True
+            urls = {}
+            for band in bandlist:
+                print "                    %s" % (band),
+                url = getUrl(run, rerun, camcol, field, band)
+                answer = testUrlExists(url)
+                if answer == -1:
+                    allExist = False
+                    print "\033[31m [Fail!] \033[0m\n"
+                    break
+                print "\033[32m [OK] \033[0m   (%i bytes)" % (answer)
+                urls[band] = url
+            if not allExist:
+                errFile.write("%1.6f  %1.6f   %s \n" % (ra, dec, url.split("/")[-1][8:-9]))
+                continue
+            downloadThreads = []
+            for band in bandlist:
+                dth = Thread(target=download, args=(urls[band], band, curGalName))
+                dth.daemon = True
+                dth.start()
+                downloadThreads.append(dth)
+            print " Downloading",
+            while threadsAlive(downloadThreads):
+                sys.stdout.write(".")
+                sys.stdout.flush()
+                sleep(0.333)
+                sys.stdout.write(".")
+                sys.stdout.flush()
+                sleep(0.333)
+                sys.stdout.write(".")
+                sys.stdout.flush()
+                sleep(0.333)
+                sys.stdout.write("\b\b\b   \b\b\b")
+                sys.stdout.flush()
+                sleep(0.333)
+            print "         \033[34m [DONE] \033[0m    (%1.2f sec)\n\n" % (time()-startTime)
+            outFile.write(" %s.fits " % (curGalName))
+        outFile.write("\n")
+        if args.script and (len(objFieldList) > 1):
+            for band in bandlist:
+                f = open("./downloads/%s/concatenate.sh" % (band), "a")
+                for i in xrange(len(objFieldList)):
+                    f.write("bunzip2 %s_%i_%s.fits.bz2 \n" % (galName, i, band))
+                f.write("swarp ")
+                for i in xrange(len(objFieldList)):
+                    f.write(" %s_%i_%s.fits[0] " % (galName, i, band))
+                f.write("\n")
+                f.write("mv ./coadd.fits ./%s_%s.fits \n" % (galName, band))
+                for i in xrange(len(objFieldList)):
+                    f.write("rm %s_%i_%s.fits \n" % (galName, i, band))
+                f.write("rm coadd.weight.fits \n \n")
+                f.close()
